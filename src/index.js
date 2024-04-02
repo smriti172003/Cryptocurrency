0847d655dffe4c242b2fda0d4d8ac2821de8d2e1
Smriti <smritimakkar173@gmail.com>
2024-04-02 13:01:07 +0530

    Service->api + app->store which contains reducer + index.js->provider along with its importation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import  ReactDOM from "react-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
 import { BrowserRouter as Router} from "react-router-dom";
 
+import store from "./app/store";
 
 
 ReactDOM.render(
     <Router>
-        <App />
+        <Provider store ={store}>
+            <App />
+        </Provider>
     </Router>,
  document.getElementById("root")
 
